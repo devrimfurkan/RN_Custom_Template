ee27a6d3633f9efa257a753f1ce8aacc7c8afdfb
Ahmet Borslan <borslanahmet@gmail.com>
2022-07-28 15:21:15 +0300

    Update App.tsx

--- TestTemplate/App.tsx
+++ TestTemplate/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, Layout, Text } from '@logo-rn/shared-utils/utils/logo-ui/components';
 import { LogoLabel } from '@logo-rn/logo-label';
 import { StyleSheet } from 'react-native'
 
 const App =() => (
   <ApplicationProvider {...eva} theme={eva.light}>
-  <LogoLabel style={styles.labels}>LogoLabel is HERE</LogoLabel>
+  <LogoLabel >LogoLabel is HERE</LogoLabel>
   </ApplicationProvider>
 );
 
@@ -16,6 +16,7 @@ export default App;
 const styles = StyleSheet.create({
   
   labels:{
+    marginTop:25,
     color:'red',
     fontSize:44,
   }
