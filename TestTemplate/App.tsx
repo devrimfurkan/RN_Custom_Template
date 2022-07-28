388875c7b248aae116e1545e3752ad33d4dd0b2a
Ahmet Borslan <borslanahmet@gmail.com>
2022-07-28 16:35:19 +0300

    divider alet added

--- TestTemplate/App.tsx
+++ TestTemplate/App.tsx
@@ -4,28 +4,56 @@ import { ApplicationProvider, Layout, Text } from '@logo-rn/shared-utils/utils/l
 import { LogoLabel } from '@logo-rn/logo-label';
 import { StyleSheet } from 'react-native';
 import {LogoButton} from '@logo-rn/logo-button';
+import {LogoAlert} from '@logo-rn/logo-alert';
+import {LogoDivider} from '@logo-rn/logo-divider';
 
 
-const App =() => (
-  
+const App =() => {
 
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <LogoButton
-     title={"Press Me"}
-     onClick={()=>console.log("Pressed")} />
-  
+  const [visible, setVisible] = React.useState(false);
+  const showAlert = async () => {
+    setVisible(true)
+}
+
+const closeAlert = async () => {
+    setVisible(false)
+}
+  return(
+    
+    <ApplicationProvider {...eva} theme={eva.light}>
+     
+      <LogoButton
+         title={"Press Me"}
+         onClick={showAlert} />
+
+        <LogoAlert
+        desc={"Description"}
+        visible={visible}
+        close={closeAlert}
+        title={"Title"}
+        rightButtonText={"close"}
+        leftButtonText={"close"}
+        rightButtonAction={closeAlert}
+        leftButtonAction={closeAlert}
+        /> 
+
+      <LogoDivider width="90%" style={styles.dividerLogo}/>
+
      </ApplicationProvider>
-);
+        )
+        
+      }
 
 export default App;
 
 
 const styles = StyleSheet.create({
   
-  labels:{
-    marginTop:25,
-    color:'red',
-    fontSize:44,
+  dividerLogo:{
+    margin:5,
+    height:5,
+    backgroundColor:'red',
+    
   }
 
 })
