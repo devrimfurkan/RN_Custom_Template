73225c6f6045efc447050ad6340ac45228d05b70
Ahmet Borslan <borslanahmet@gmail.com>
2022-07-28 14:06:55 +0300

    28

--- TestTemplate/App.tsx
+++ TestTemplate/App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 import { LogoLabel } from '@logo-rn/logo-label';
-
-const HomeScreen = () => (
-  <Layout >
-    <Text category='h1'>HOME Page</Text>
-    <LogoLabel children=''>LogoLabel is HERE</LogoLabel>
-  </Layout>
-);
+import { StyleSheet } from 'react-native'
 
 const App =() => (
   <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
+  <LogoLabel style={styles.labels}>LogoLabel is HERE</LogoLabel>
   </ApplicationProvider>
 );
 
 export default App;
+
+
+const styles = StyleSheet.create({
+  
+  labels:{
+    color:'red',
+    fontSize:44,
+  }
+
+})
