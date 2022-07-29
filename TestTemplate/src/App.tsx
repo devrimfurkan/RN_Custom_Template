47ff160372b9c85b737e5dfcc2f5d9f878dd62cc
Devrim Furkan GUNER <70522562+devrimfurkan@users.noreply.github.com>
2022-07-29 14:16:58 +0300

    Attention menu componet core
    this component working still not bad but when we went to the menu component core we got some issues at above the returns visible component. When we looked this props text underline is red. so this problem is "No overload matches" but we could not fix it.

--- TestTemplate/src/App.tsx
+++ TestTemplate/src/App.tsx
@@ -1,12 +1,43 @@
 import React, { useState } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, } from '@logo-rn/shared-utils/utils/logo-ui/components';
-import { StyleSheet } from 'react-native';
-import {LogoListItem} from '@logo-rn/logo-list-item';
+import { StyleSheet, Text } from 'react-native';
+import { LogoMenu } from '@logo-rn/logo-menu';
 const App = () => {
+
+  const [visible, setVisible] = useState(false)
+  const MENU_LIST: { id: string; title: string; selected: boolean; }[] = [
+    {
+      id: "1",
+      title: "Menu Option1",
+      selected: false
+    },
+    {
+      id: "2",
+      title: "Menu Option2",
+      selected: false
+    },
+    {
+      id: "3",
+      title: "Menu Option3",
+      selected: false
+    },
+    {
+      id: "4",
+      title: "Menu Option4",
+      selected: false
+    }
+  ]
+  
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
-        <LogoListItem columnName='title' item={""} onClick={() => console.log("")} />
+      <LogoMenu
+        anchor={()=><Text>"denee"</Text>}
+        optionList={MENU_LIST}
+        visible={visible}
+        close={() => setVisible(false)}
+        setOptionList={(options: any) =>console.log("denee")}
+        isRightIcon={true} />
     </ApplicationProvider>
   )
 }
