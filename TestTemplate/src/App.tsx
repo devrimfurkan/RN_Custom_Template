d830e0c508ffb25dfe94228272c34d692bda4a73
Devrim Furkan GUNER <70522562+devrimfurkan@users.noreply.github.com>
2022-07-29 13:16:02 +0300

    could not found any issue

--- TestTemplate/src/App.tsx
+++ TestTemplate/src/App.tsx
@@ -2,12 +2,31 @@ import React, { useState } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, } from '@logo-rn/shared-utils/utils/logo-ui/components';
 import { StyleSheet } from 'react-native';
-import {LogoFabButton} from '@logo-rn/logo-fab-button';
+import {LogoTextInput, LogoDecimalInput, LogoPasswordInput} from '@logo-rn/logo-text-input';
 
 const App = () => {
+  const [value, setValue] = React.useState('');
+  const [valueDeciaml, setValueDecimal] = React.useState(0);
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
-      <LogoFabButton onClick={()=>{console.log("clicked...")}}/>
+             <LogoTextInput 
+          label="Text Here"
+          placeholder={"Yazı Giriniz"}
+          value={value}
+          onChangeValue={(nextValue: any) => setValue(nextValue)} 
+        />
+               <LogoDecimalInput  
+            label="Number Here"
+            placeholder={"Sayı Giriniz"}
+            value={value}
+            onChangeValue={(nextValue: any) => setValue(nextValue)} 
+         />
+                <LogoPasswordInput 
+          label="Password Here"
+          placeholder={"Sayı Giriniz"}
+          value={value}       
+          onChangeValue={(nextValue: any) => setValue(nextValue)} />
+
     </ApplicationProvider>
   )
 }
