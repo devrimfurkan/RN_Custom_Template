ad5e4211e936aab77e43d9eac5dfd327a1c6cc60
Devrim Furkan GUNER <70522562+devrimfurkan@users.noreply.github.com>
2022-07-29 11:32:11 +0300

    date-table not fixed
    probably this components props not include setList props.
    error: Type '{ list: { test: string; }[]; setList: (list: any) => void; listColumnKey: string; onClick: () => void; }' is not assignable to type 'IntrinsicAttributes & Props'.
      Property 'setList' does not exist on type 'IntrinsicAttributes & Props'.

--- TestTemplate/src/App.tsx
+++ TestTemplate/src/App.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, } from '@logo-rn/shared-utils/utils/logo-ui/components';
 import { StyleSheet } from 'react-native';
-import {LogoChips} from '@logo-rn/logo-chips';
+import { LogoDataTable } from '@logo-rn/logo-data-table';
 
-const App =() => {
-  return(
+const App = () => {
+  const LIST_ITEMS=[{
+    test: "This is Test item"
+  }]
+  const [list, setList] = useState(LIST_ITEMS)
+  return (
     <ApplicationProvider {...eva} theme={eva.light}>
-        <LogoChips status={"primary"} label={"Chips"} />
-     </ApplicationProvider>
-        )
-      }
+      <LogoDataTable 
+      list={list} 
+      setList={(list: any) => setList(list)} 
+      listColumnKey="title"
+      onClick={()=>{}}
+
+      />
+    </ApplicationProvider>
+  )
+}
 
 export default App;
 
 
 const styles = StyleSheet.create({
-  
-  dividerLogo:{
-    margin:5,
-    height:5,
-    fontSize:33,
-    backgroundColor:'red',
-    
+
+  dividerLogo: {
+    margin: 5,
+    height: 5,
+    fontSize: 33,
+    backgroundColor: 'red',
+
   }
 
 })
