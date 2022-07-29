ab028cf6298be7750623a3b8a57a59e6456933f6
Devrim Furkan GUNER <70522562+devrimfurkan@users.noreply.github.com>
2022-07-29 14:55:14 +0300

    could find two issue
    first error solution;
    when we went to the core component spinner we put correct assets path
    
    second ono;
    Error TypeError: null is not an object (evaluating 'safeGetViewManagerConfig('LottieAnimationView').Commands')
    
    we got this error and soulition is
    in the project path terminal we wrote " npm link " code

--- TestTemplate/src/App.tsx
+++ TestTemplate/src/App.tsx
@@ -2,16 +2,13 @@ import React, { useState } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, } from '@logo-rn/shared-utils/utils/logo-ui/components';
 import { StyleSheet, Text } from 'react-native';
-import { LogoSlider, LogoMultiSlider } from '@logo-rn/logo-slider';
+import {LogoSpinner} from '@logo-rn/logo-spinner';
 
 const App = () => {
   const [value, setValue] = useState(100)
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
-      <LogoSlider
-        value={value}
-        setValue={(newValue: number) => setValue(newValue)}
-      />
+       <LogoSpinner title="Loading"/>
     </ApplicationProvider>
   )
 }
