3f2ef9f7b90cf1d2f384e7279c8ca2cdae58970c
Ahmet Borslan <borslanahmet@gmail.com>
2022-07-29 14:15:55 +0300

    Radio Button Usage

--- TestTemplate/src/App.tsx
+++ TestTemplate/src/App.tsx
@@ -6,38 +6,41 @@ import { StyleSheet } from 'react-native';
 import {LogoButton} from '@logo-rn/logo-button';
 import {LogoAlert} from '@logo-rn/logo-alert';
 import {LogoDivider} from '@logo-rn/logo-divider';
+import { LogoRadioButton, LogoRadioButtonGroup } from '@logo-rn/logo-radio-button';
 
 
 const App =() => {
 
-  const [visible, setVisible] = React.useState(false);
-  const showAlert = async () => {
-    setVisible(true)
-}
-
-const closeAlert = async () => {
-    setVisible(false)
-}
+  const RADIO_OPTIONS: { id: string, value: string, checked: boolean, disabled: boolean }[] = [
+    {
+          id: "1",
+          value: "Option 1",
+          checked: false,
+          disabled: false
+      },
+      {
+          id: "2",
+          value: "Option 2",
+          checked: true,
+          disabled: false
+      },
+      {
+          id: "3",
+          value: "Option 3",
+          checked: true,
+          disabled: true
+      },
+    ]
   return(
     
     <ApplicationProvider {...eva} theme={eva.light}>
-     <LogoLabel>dsafsd</LogoLabel>
-      <LogoButton
-         title={"Press Me"}
-         onClick={showAlert} />
+       <LogoRadioButtonGroup options={RADIO_OPTIONS} title={"Options"} />
 
-        <LogoAlert
-        desc={"Description"}
-        visible={visible}
-        close={closeAlert}
-        title={"Title"}
-        rightButtonText={"close"}
-        leftButtonText={"close"}
-        rightButtonAction={closeAlert}
-        leftButtonAction={closeAlert}
-        /> 
 
-      <LogoDivider width="90%" style={styles.dividerLogo}/>
+       <LogoRadioButton checked={false} key={"1"} label={"1"} />
+       <LogoRadioButton checked={false} disabled={true} key={"2"} label={"2"} />
+       <LogoRadioButton checked={true} key={"3"} label={"3"}/>
+       <LogoRadioButton checked={true} disabled={true} key={"4"} label={"4"}/>
 
      </ApplicationProvider>
         )
