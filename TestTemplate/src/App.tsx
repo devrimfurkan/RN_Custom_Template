51a8b94eb395db15c909210e1f1330bdfd08e1f0
Devrim Furkan GUNER <70522562+devrimfurkan@users.noreply.github.com>
2022-07-29 09:56:39 +0300

    Logo Avatar

--- TestTemplate/src/App.tsx
+++ TestTemplate/src/App.tsx
@@ -1,44 +1,16 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@logo-rn/shared-utils/utils/logo-ui/components';
-import { LogoLabel } from '@logo-rn/logo-label';
+import { ApplicationProvider, } from '@logo-rn/shared-utils/utils/logo-ui/components';
+import {LogoAvatar} from '@logo-rn/logo-avatar';
 import { StyleSheet } from 'react-native';
-import {LogoButton} from '@logo-rn/logo-button';
-import {LogoAlert} from '@logo-rn/logo-alert';
-import {LogoDivider} from '@logo-rn/logo-divider';
+
 
 
 const App =() => {
-
-  const [visible, setVisible] = React.useState(false);
-  const showAlert = async () => {
-    setVisible(true)
-}
-
-const closeAlert = async () => {
-    setVisible(false)
-}
   return(
     
     <ApplicationProvider {...eva} theme={eva.light}>
-     <LogoLabel>dsafsd</LogoLabel>
-      <LogoButton
-         title={"Press Me"}
-         onClick={showAlert} />
-
-        <LogoAlert
-        desc={"Description"}
-        visible={visible}
-        close={closeAlert}
-        title={"Title"}
-        rightButtonText={"close"}
-        leftButtonText={"close"}
-        rightButtonAction={closeAlert}
-        leftButtonAction={closeAlert}
-        /> 
-
-      <LogoDivider width="90%" style={styles.dividerLogo}/>
-
+     <LogoAvatar size={56} />
      </ApplicationProvider>
         )
         
@@ -52,6 +24,7 @@ const styles = StyleSheet.create({
   dividerLogo:{
     margin:5,
     height:5,
+    fontSize:33,
     backgroundColor:'red',
     
   }
